f5774d4bb43e337a5fe80e85ba20441efc3feaec
xjbeta <xjbeta.tiny@gmail.com>
2021-09-06 18:04:11 +0800

    Instructions.

--- firefox.swift
+++ firefox.swift
@@ -10,9 +10,13 @@ func shell(_ command: String) -> Int32 {
     task.arguments = ["-c", command]
     task.launch()
     task.waitUntilExit()
-    
+
     return task.terminationStatus
 }
+
+let firefoxPath = "/Applications/Firefox.app"
+let omnijaPath = firefoxPath + "/Contents/Resources/browser/omni.ja"
+let tmp = NSTemporaryDirectory() + "firefox.omni.ja"
 
 func fileExists(_ path: String) -> Bool {
     return FileManager.default.fileExists(atPath: path)
@@ -21,11 +25,6 @@ func fileExists(_ path: String) -> Bool {
 func clearTmp() {
     try? FileManager.default.removeItem(atPath: tmp)
 }
-
-let firefoxPath = "/Applications/Firefox.app"
-let omnijaPath = firefoxPath + "/Contents/Resources/browser/omni.ja"
-
-let tmp = NSTemporaryDirectory() + "firefox.omni.ja"
 
 print("\(omnijaPath)")
 if !fileExists(omnijaPath) {
@@ -36,7 +35,7 @@ if !fileExists(omnijaPath) {
 print("\(tmp)")
 clearTmp()
 
-    
+
 
 shell("unzip -q \(omnijaPath) -d \(tmp)")
 //shell("unzip \(omnijaPath) -d \(tmp)")
@@ -90,12 +89,22 @@ if let content = FileManager.default.contents(atPath: file2),
 }
 
 let bak = omnijaPath + ".bak"
+let new = omnijaPath + ".new"
 
 shell("mv \(omnijaPath) \(bak)")
 print("backup omni.ja to \(bak)")
 
-shell("cd \(tmp); zip -qr9XD \(omnijaPath) *")
-print("zip omni.ja to \(omnijaPath)")
+shell("cd \(tmp); zip -qr9XD \(new) *")
+print("zip omni.ja to \(new)")
 
 clearTmp()
 print("clear tmp files.")
+
+
+print("1. Open Firefox.app")
+print("2. Click 'Quit Firefox'")
+
+let path = NSString(string: omnijaPath).deletingLastPathComponent
+
+print("3. Go to this path: \(path)")
+print("4. Rename 'omni.ja.new' to 'omni.ja'")
